04917bee4a9797e7b53592e1694de0540b46c56f
Jpbroadvision <jpbroadvision2@gmail.com>
2021-12-15 13:47:55 +0000

    style: Replace all empty print() with \n

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -71,8 +71,7 @@ def get_filters():
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         try:
-            print()
-            city = (input('Enter name of city (chicago, new york city, washington):>> ').strip()).lower()
+            city = (input('\nEnter name of city (chicago, new york city, washington):>> ').strip()).lower()
             # Get the name of the city the user entered and check whether it exist
             response = city in CITIES_ONLY
             # If the responses exist, then pass
@@ -89,23 +88,19 @@ def get_filters():
                     city = switch_to_number(int_city, 'city')
                     break
                 except KeyError:
-                    print()
-                    print("The input provided is not available. If it is a number you provided, select between 1, 2 or 3")
+                    print("\nThe input provided is not available. If it is a number you provided, select between 1, 2 or 3")
                     print()
             
         except ValueError:
-            print()
-            print(":) NOOOOOOOOO!!!. Try again.  Enter a valid city name or number to continue...")
+            print("\n:) NOOOOOOOOO!!!. Try again.  Enter a valid city name or number to continue...")
         except KeyboardInterrupt:
             return
-    print()
-    print(f"GREAT, YOU SELECTED {city}")
+    print(f"\nGREAT, YOU SELECTED {city}")
         # get user input for month (all, january, february, ... , june)
     while True:
         try:
-            print()
             month = (
-                input('Enter a month (all, january, february, ... , june):>> ').strip()).lower()
+                input('\nEnter a month (all, january, february, ... , june):>> ').strip()).lower()
             # Get the name of the month the user entered and check whether it exist
             response = month in MONTHS_ONLY
             # If the responses exist, then pass
@@ -115,29 +110,26 @@ def get_filters():
             elif response == False:
                 try:
                     # We alert the user to enter a number or an integer between 0, 1, 2, 3, 4, 5 or 6 respectively
-                    print()
+                    
                     int_month = int(input(
-                        'Oops!!. There may be a typo in there. You use can enter 0, 1, 2, 3, 4, 5 or 6 for (all, january, february, ... , june) respectively:>> '))
+                        '\nOops!!. There may be a typo in there. You use can enter 0, 1, 2, 3, 4, 5 or 6 for (all, january, february, ... , june) respectively:>> '))
                     # Check whether what the user entered is a number, may raise KeyError 
                     month = switch_to_number(int_month, 'month')
                     break
                 except KeyError:
-                    print()
-                    print("The input provided is not available. If it is a number you provided, select between 0, 1, 2, 3, 4, 5 or 6 for (all, january, february, ... , june)")
-                    print()
+                    print("\nThe input provided is not available. If it is a number you provided, select between 0, 1, 2, 3, 4, 5 or 6 for (all, january, february, ... , june)\n")
+                    
         except ValueError:
-            print()
-            print(":) NOOOOOOOOO!!!  Try again. Enter a valid month name or number to continue...")
+            print("\n:) NOOOOOOOOO!!!  Try again. Enter a valid month name or number to continue...")
         except KeyboardInterrupt:
-            return
-    print()  
-    print(f"YOU ARE ALMOST THERE. YOU SELECTED {month}")
+            return 
+    print(f"\nYOU ARE ALMOST THERE. YOU SELECTED {month}")
         
 
     while True:
         # get user input for day of week (all, monday, tuesday, ... sunday)
         try:
-            day = (input('Enter a day in a week (all, monday, tuesday, ... sunday):>> ').strip()).lower()
+            day = (input('\nEnter a day in a week (all, monday, tuesday, ... sunday):>> ').strip()).lower()
             response = day in WEEK_DAYS_ONLY
             # If the responses exist, then pass
             if response == True:
@@ -146,26 +138,23 @@ def get_filters():
             elif response == False:
                 try:
                     # We alert the user to enter a number or an integer between 0, 1, 2, 3, 4, 5, 6 or 7 for (all, monday, tuesday, ... sunday) respectively
-                    print()
-                    int_day = int(input('Oops!!. There may be a typo in there. \
+                    
+                    int_day = int(input('\nOops!!. There may be a typo in there. \
                         You use can enter 0, 1, 2, 3, 4, 5, 6 or 7 for (all, monday, tuesday, ... sunday) respectively:>> '))
                     # Check whether what the user entered is a number, may raise KeyError 
                     day = switch_to_number(int_day, 'day')
                     break
                 except KeyError:
-                    print()
-                    print("The input provided is not available. If it is a number you provided, select between 0, 1, 2, 3, 4, 5, 6 or 7 for (all, monday, tuesday, ... sunday)")
-                    print()
+                    print("\nThe input provided is not available. If it is a number you provided, select between 0, 1, 2, 3, 4, 5, 6 or 7 for (all, monday, tuesday, ... sunday)\n")
+                    
         except ValueError:
-            print()
-            print(":) NOOOOOOOOO!!!  Try again. Enter a valid day name or number to continue...")
+            print("\n:) NOOOOOOOOO!!!  Try again. Enter a valid day name or number to continue...")
         except KeyboardInterrupt:
             return
-    print()
-    print(f"EXECELLENT!. YOU ARE ON TRACK. YOU SELECTED {day}")
+    print(f"\nEXECELLENT!. YOU ARE ON TRACK. YOU SELECTED {day}")
     
     print('-'*40)
-    print(f"YOUR SEARCH QUERIES ARE CITY: {city}, MONTH: {month} and DAY: {day} ")
+    print(f"\nYOUR SEARCH QUERIES ARE CITY: {city}, MONTH: {month} and DAY: {day} \n")
     return city, month, day
 
 
@@ -219,19 +208,17 @@ def time_stats(read_city):
 
     # display the most common month
     common_month = read_city['month'].mode()[0]
-    print()
-    print(f'\nBase on your Selections. The most common month of travel is: {MONTHS_ONLY[common_month].title()}')
+    print(f'\nBase on your Selections. The most common month of travel is: {MONTHS_ONLY[common_month].title()}\n')
 
     # display the most common day of week
     common_day = read_city['day_of_week'].mode()[0]
-    print()
-    print(f'\nBase on your Selections. The most common day of the week used of travel is: {common_day}')
+   
+    print(f'\nBase on your Selections. The most common day of the week used of travel is: {common_day}\n')
     # display the most common start hour
     common_hour = read_city['hour'].mode()[0]
-    print()
-    print(f'\nBase on your Selections. The most common hour is: {common_hour}')
+    print(f'\nBase on your Selections. The most common hour is: {common_hour}\n')
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
+    print("\nThis took %s seconds.\n" % (time.time() - start_time))
     print('-'*40)
 
 
@@ -248,18 +235,16 @@ def station_stats(read_city):
 
     # display most commonly used start station
     common_use_start_station = read_city['Start Station'].mode()[0]
-    print()
-    print(f"The most commonly used start station from the given selections is:  {common_use_start_station}")
+   
+    print(f"\nThe most commonly used start station from the given selections is:  {common_use_start_station}\n")
     # display most commonly used end station
     common_use_end_station = read_city['End Station'].mode()[0]
-    print()
-    print(f"The most commonly used end station from the given fitered data is: {common_use_end_station}")
+    print(f"\nThe most commonly used end station from the given fitered data is: {common_use_end_station}\n")
     # display most frequent combination of start station and end station trip
     start_end_station = (read_city['Start Station'] + '||' + read_city['End Station']).mode()[0]
-    print()
-    print(f"The most frequent combination of start station and end station trip is :  {start_end_station.split('||')}")
+    print(f"\nThe most frequent combination of start station and end station trip is :  {start_end_station.split('||')}\n")
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
+    print("\nThis took %s seconds.\n" % (time.time() - start_time))
     print('-'*40)
 
 
@@ -275,15 +260,13 @@ def trip_duration_stats(read_city):
 
     # display total travel time
     total_travel_time = read_city['Trip Duration'].sum()
-    print()
-    print(f"The total travel time selections is: {total_travel_time}")
+    print(f"The total travel time selections is: {total_travel_time}\n")
 
     # display mean travel time
     mean_travel_time = read_city['Trip Duration'].mean()
-    print()
-    print(f"The mean travel time selections is: {mean_travel_time}")
+    print(f"\nThe mean travel time selections is: {mean_travel_time}\n")
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
+    print("\nThis took %s seconds.\n" % (time.time() - start_time))
     print('-'*40)
 
 
@@ -300,36 +283,29 @@ def user_stats(read_city, city):
 
     # Display counts of user types
     count_user_types = read_city['User Type'].value_counts()
-    print()
-    print(f"The total counts of user types from selections is:\n{count_user_types}")
+    print(f"\nThe total counts of user types from selections is:\n{count_user_types}")
     # Display counts of gender
     if 'Gender' in read_city.columns:
         count_gender = read_city['Gender'].value_counts()
-        print()
-        print(f"The count of user gender from Selection is:\n{count_gender}")
+        print(f"\nThe count of user gender from Selection is:\n{count_gender}")
     else:
-        print()
-        print(f"The count of user gender from {city} city is not possible because it has no such column as 'Gender'. Skipping now...")
+        print(f"\nThe count of user gender from {city} city is not possible because it has no such column as 'Gender'. Skipping now...\n")
 
 
     # Display earliest, most recent, and most common year of birth
     if 'Birth Year' in read_city.columns:
         # Earliest year of birth
         earliest_birth = read_city['Birth Year'].min()
-        print()
-        print(f"Earliest birth from Selection is: {earliest_birth}")
+        print(f"\nEarliest birth from Selection is: {earliest_birth}\n")
         # Most recent year of birth
         most_recent_birth = read_city['Birth Year'].max()
-        print()
-        print(f"Most recent birth from Selection is: {most_recent_birth}")
+        print(f"\nMost recent birth from Selection is: {most_recent_birth}\n")
         # Most common year of birth
         most_common_birth = read_city['Birth Year'].mode()[0]
-        print()
-        print(f"Most common birth from Selection is: {most_common_birth}")
+        print(f"\nMost common birth from Selection is: {most_common_birth}\n")
     else:
-        print()
-        print(f"The count stats for Birth Year from {city} city is not possible because it has no such column as 'Birth Year'. Skipping now...")
-    print("\nThis took %s seconds." % (time.time() - start_time))
+        print(f"\nThe count stats for Birth Year from {city} city is not possible because it has no such column as 'Birth Year'. Skipping now...\n")
+    print("\nThis took %s seconds.\n" % (time.time() - start_time))
     print('-'*40)
     print("D O N E")
 
@@ -340,14 +316,12 @@ def user_view_raw_data(read_city):
         (DataFrame) read_city - Pandas DataFrame containing city data with filters.
     """
     while True:
-        print()
-        view_raw_data = input('Would you like to view first five row of raw data? Enter yes or no.\n')
+        view_raw_data = input('\nWould you like to view first five row of raw data? Enter yes or no.\n')
         if view_raw_data.lower() == 'yes':
             print(read_city.head())
             initial_view = 0
             while True:
-                print()
-                view_raw_data = input('Would you like to view the next five raw data set from this city? Enter yes or no. >>')
+                view_raw_data = input('\nWould you like to view the next five raw data set from this city? Enter yes or no. >>')
                 if view_raw_data.lower() == 'yes' or view_raw_data.lower() == 'y':
                     initial_view += 5
                     print(read_city.iloc[initial_view : initial_view + 5])
